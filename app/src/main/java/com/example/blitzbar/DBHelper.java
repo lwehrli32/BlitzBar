1cac663dd4f558c1e9e3ba4014912bf38e725e03
lukas <lwehrli@wisc.edu>
2021-10-18 16:20:41 -0500

    Creating sign in page

---
+++ app/src/main/java/com/example/blitzbar/DBHelper.java
@@ -0,0 +1,49 @@
+package com.example.blitzbar;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBHelper {
+    SQLiteDatabase sqLiteDatabase;
+
+    public DBHelper(SQLiteDatabase sqLiteDatabase){
+        this.sqLiteDatabase = sqLiteDatabase;
+    }
+
+    public void createUsersTable(){
+        sqLiteDatabase.execSQL("Create table if not exists users " + "(user_id Integer primary key, username text, first_name text, last_name text, email text, birthday text, blitz_score Integer, fav_drink text, fav_bar text, dark_mode Integer, search_radius Integer)");
+    }
+
+    public boolean insertUser(String username, String first_name, String last_name, String email, String birthday, long blitz_score, String fav_drink, String fav_bar){
+        createUsersTable();
+
+        // check if the user already exists
+        Cursor c = sqLiteDatabase.rawQuery(String.format("Select * from users where username like '%s'", username), null);
+        int numUsers = 0;
+
+        while(!c.isAfterLast()){
+            numUsers++;
+            c.moveToNext();
+        }
+
+        c.close();
+        sqLiteDatabase.close();
+
+        if(numUsers > 0) return false;
+
+        // user does not exist, insert the user
+        sqLiteDatabase.execSQL(String.format("Insert into users (username, first_name, last_name, email, birthday, blitz_score, fav_drink, fav_bar, dark_mode, search_radius) Values ('%s', '%s', '%s', '%s', '%s', '%d', '%s', '%s', 0, 5", username, first_name, last_name, email, birthday, blitz_score, fav_drink, fav_bar));
+        return true;
+    }
+
+    public void updateUser(){
+        createUsersTable();
+        // TODO update users table
+    }
+
+    public User getUser(String username){
+        Cursor c = sqLiteDatabase.rawQuery(String.format("Select * from users where username like '%s'", username), null);
+        //User user = new User();
+        return Null;
+    }
+}
---
+++ app/src/main/java/com/example/blitzbar/User.java
@@ -0,0 +1,52 @@
+package com.example.blitzbar;
+
+public class User {
+
+    private String username;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String birthday;
+    private long blitz_score;
+    private String fav_bar;
+    private String fav_drink;
+    private boolean dark_mode;
+    private int search_radius;
+
+    public User(String username, String first_name, String last_name, String email, String birthday, long blitz_score, String fav_bar, String fav_drink, int dark_mode, int search_radius){
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.birthday = birthday;
+        this.blitz_score = blitz_score;
+        this.fav_bar = fav_bar;
+        this.fav_drink = fav_drink;
+        this.search_radius = search_radius;
+        if(dark_mode == 0){
+            this.dark_mode = false;
+        }else{
+            this.dark_mode = true;
+        }
+    }
+
+    public String getUsername(){return this.username;}
+
+    public String getFirst_name(){return this.first_name;}
+
+    public String getLast_name(){return this.last_name;}
+
+    public String getEmail(){return this.email;}
+
+    public String getBirthday(){return this.birthday;}
+
+    public String getFav_bar(){return this.fav_bar;}
+
+    public String getFav_drink(){return this.fav_drink;}
+
+    public Long getBlitz_score(){return this.blitz_score;}
+
+    public int getSearch_radius(){return this.search_radius;}
+
+    public boolean getDark_mode(){return this.dark_mode;}
+}
