6a218b393e54265b2e1aaa1d6968b9bc53e72ec5
ArisG <mrshabushika10@gmail.com>
2021-11-16 15:06:50 -0600

    Settings Activity Method Setup

--- app/src/main/java/com/example/blitzbar/SettingsActivity.java
+++ app/src/main/java/com/example/blitzbar/SettingsActivity.java
@@ -1,13 +1,48 @@
 package com.example.blitzbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
     SharedPreferences sp;
+
+    // TODO CREATE METHODS FOR SWITCHES AND BUTTONS
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.backButton) {
+            goToLastActivity();
+        }
+    }
+
+    public void onSwitch(View v) {
+        switch (v.getId()) {
+            case R.id.DarkMode:
+                toggleDarkMode();
+                break;
+            case R.id.Sounds:
+                // TODO toggle sounds
+                break;
+            case R.id.Notifications:
+                // TODO toggle notifications
+                break;
+            case R.id.LocationPublic:
+                // TODO toggle publicized location
+                break;
+        }
+    }
+
+    public void goToLastActivity() {
+
+    }
+
+    public void toggleDarkMode() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
