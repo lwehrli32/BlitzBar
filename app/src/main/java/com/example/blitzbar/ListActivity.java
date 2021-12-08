f4594532240072b897374ae5d43ba5c6782fe152
Jake DeWitte <jdewitte@wisc.edu>
2021-12-08 04:48:08 -0600

    Working on finding nearby bars

--- app/src/main/java/com/example/blitzbar/ListActivity.java
+++ app/src/main/java/com/example/blitzbar/ListActivity.java
@@ -2,15 +2,19 @@ package com.example.blitzbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.material.navigation.NavigationBarView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ListActivity extends AppCompatActivity {
 
     private NavigationBarView bottomNavigationBarView;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,23 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
         bottomNavigationBarView = findViewById(R.id.bottomnav);
         bottomNavigationBarView.setOnItemSelectedListener(bottomnavFunction);
+
+
     }
+
+    public void listBars(View v) {
+        StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        stringBuilder.append("&radius="+1000);
+        stringBuilder.append("&keyword="+"bar");
+        stringBuilder.append("&key="+getResources().getString(R.string.google_places_key));
+
+        String url = stringBuilder.toString();
+
+        Object dataTransfer[] = new Object[2];
+        dataTransfer[0] = mMap;
+
+    }
+
 
     private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
         @Override
