30a42608bda2b24f383cbdfb9fe59a7d9a45ba09
Jake DeWitte <jdewitte@wisc.edu>
2021-12-13 08:09:38 -0600

    Working on friends

---
+++ app/src/main/java/com/example/blitzbar/FindFriends.java
@@ -0,0 +1,59 @@
+package com.example.blitzbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class FindFriends extends AppCompatActivity {
+
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sp = getApplicationContext().getSharedPreferences("BlitzBar", Context.MODE_PRIVATE);
+        boolean isDarkMode = sp.getInt("isDarkMode", 0) == 1;
+        if (isDarkMode) {
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+        NavigationBarView bottomNavigationBarView = findViewById(R.id.bottomnav);
+        bottomNavigationBarView.setOnItemSelectedListener(bottomnavFunction);
+    }
+
+    private final NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent menu_intent = new Intent();
+            switch (item.getItemId()) {
+                case R.id.settings:
+                    menu_intent = new Intent(FindFriends.this, SettingsActivity.class);
+                    break;
+                case R.id.listView:
+                    menu_intent = new Intent(FindFriends.this, ListActivity.class);
+                    break;
+                case R.id.mapView:
+                    menu_intent = new Intent(FindFriends.this, MapsActivity.class);
+                    break;
+                default:
+                    return false;
+            }
+
+
+            startActivity(menu_intent);
+            return true;
+        }
+    };
+}
---
+++ app/src/main/java/com/example/blitzbar/Users.java
@@ -0,0 +1,4 @@
+package com.example.blitzbar;
+
+public class Users {
+}
