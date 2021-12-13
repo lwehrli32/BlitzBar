8b0777847317a71171869be484b9184f871b4442
Aristidis10 <mrshabushika10@gmail.com>
2021-12-13 06:20:40 -0600

    Firebase User Creation User Check Update Final

--- app/src/main/java/com/example/blitzbar/FireBaseHelper.java
+++ app/src/main/java/com/example/blitzbar/FireBaseHelper.java
@@ -24,8 +24,8 @@ public class FireBaseHelper {
 
 
     public void insertUser(String first_name, String last_name, String email, String birthday, long blitz_score, long longitude, long latitude, String password){
-        User user = new User(first_name, last_name, email, birthday, blitz_score, longitude, latitude, password);
-        userDatabase.child("users").child(String.valueOf(email.hashCode())).setValue(user);
+        User insertUser = new User(first_name, last_name, email, birthday, blitz_score, longitude, latitude, password);
+        userDatabase.child("users").child(String.valueOf(email.hashCode())).setValue(insertUser);
     }
 
     private boolean addFriend(String user_email, String friend_email){
@@ -56,10 +56,6 @@ public class FireBaseHelper {
         return new long[]{longitude, latitude};
     }
 
-    public void userExistUpdate(boolean bool){
-        userExists = bool;
-    }
-
     public boolean checkUser(String checkEmail) {
         DatabaseReference userData = userDatabase.child("users").child(String.valueOf(checkEmail.hashCode()));
         String inputUserId = String.valueOf(checkEmail.hashCode());
@@ -67,7 +63,8 @@ public class FireBaseHelper {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String userId = snapshot.getKey();
-                userExistUpdate(userId.equals(inputUserId));
+                FireBaseHelper fireBaseHelper = new FireBaseHelper(userDatabase);
+                fireBaseHelper.userExists = userId.equals(inputUserId);
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -82,6 +79,7 @@ public class FireBaseHelper {
         userData.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                FireBaseHelper fireBaseHelper = new FireBaseHelper(userDatabase);
                 String firstname = snapshot.child("first_name").toString();
                 String lastname = snapshot.child("last_name").toString();
                 String userEmail = snapshot.child("email").toString();
@@ -90,8 +88,8 @@ public class FireBaseHelper {
                 long longitude = Long.parseLong(snapshot.child("longitude").toString());
                 long latitude = Long.parseLong(snapshot.child("latitude").toString());
                 String password = snapshot.child("password").toString();
-                user = new User(firstname, lastname, userEmail, birthday, blitzScore, longitude, latitude, password);
-                if (user == null) {
+                fireBaseHelper.user = new User(firstname, lastname, userEmail, birthday, blitzScore, longitude, latitude, password);
+                if (fireBaseHelper.user == null) {
                     Log.e("user null check, first", "USER IS NULL");
                 }
             }
